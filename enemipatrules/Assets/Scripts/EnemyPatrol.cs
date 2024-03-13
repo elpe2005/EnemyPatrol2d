07fd03f90e201612c30b92dd4ab62d3878d4fd2f
elpe2005 <Epedersen2005@gmail.com>
2024-03-13 14:14:35 +0100

    Fortsatt, FOV, Waypoints

--- enemipatrules/Assets/Scripts/EnemyPatrol.cs
+++ enemipatrules/Assets/Scripts/EnemyPatrol.cs
@@ -7,22 +7,18 @@ public class EnemyPatrol : MonoBehaviour
     public Transform[] patrolPoints;
     public int targetPoint;
     public float speed;
-    // Start is called before the first frame update
     void Start()
     {
         targetPoint = 0;
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (transform.position == patrolPoints[targetPoint].position)
         {
             increaseTargetInt();
         }
-        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position,speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position,speed * Time.deltaTime); 
     }
-
     void increaseTargetInt()
     {
         targetPoint++;
---
+++ enemipatrules/Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed;
+    void Update()
+    {
+        transform.LookAt(Vector3.forward);
+    }
+}
