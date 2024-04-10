579746227fb42ea95ac78e2954b2d54e35bb1081
Elliot <Epedersen2005@gmail.com>
2024-04-10 14:14:34 +0200

    Ändrat lite

--- enemipatrules/Assets/Scripts/State.cs
+++ enemipatrules/Assets/Scripts/State.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum AiState{
-    Patrol,
-    Chase,
-}
+
 public class State : MonoBehaviour
 {
+    public enum AiState
+    {
+        Chase,
+        Patrol
+    }
+   // public void Start()
+   // {
+   //     AiState.Patrol
+   // }
+    public AiState EState;
     void Update()
     {
  
     }
     public void ChangeState()
     {
-        
+
     }
 }
