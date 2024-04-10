c9e81c3e4e76f129757c7ac80c31d921be032a3a
Mahdiiii4 <abod98451@gmail.com>
2024-04-10 14:16:56 +0200

    Nästan klar med menyer

---
+++ enemipatrules/Assets/Scripts/installngar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class installngar : MonoBehaviour
+{
+    public void installngaar()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+}
