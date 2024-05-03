c1f89507df3a0f5eccc57cb8eca2a9a87772e2a4
KungeniDjunglen <thhe05001@utb.vaxjo.se>
2024-05-03 23:33:52 +0200

    Fixat det sissta och byggdfe projektet

--- enemipatrules/Assets/Scripts/State.cs
+++ enemipatrules/Assets/Scripts/State.cs
@@ -7,19 +7,14 @@ public class State : MonoBehaviour
 {
     EnemyPatrol patrolScript;
     AiChase chaseScript;
-    FieldOfView fovScript;
     void Start()
     {
-        fovScript = GetComponent<FieldOfView>();
         patrolScript = GetComponent<EnemyPatrol>();
         chaseScript = GetComponent<AiChase>();
         chaseScript.enabled = false;
     }
     void Update()
     {
-       if(fovScript.CanSeePlayer){        
-        chaseScript.enabled = true;
-        patrolScript.enabled = false;
-       }
+       
     }
 }
